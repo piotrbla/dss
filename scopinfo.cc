f51651afff0d46db3009166863e36022cdf8cbe0
Piotr B <piotrb@gmail.com>
2021-06-29 00:30:16 +0200

    Showing only read after write dependencies

--- scopinfo.cc
+++ scopinfo.cc
@@ -49,8 +49,8 @@ ScopInfo::ScopInfo(pet_scop* scop)
 }
 ScopInfo::~ScopInfo()
 {
-    //isl_union_map_free(reads);
-    //isl_union_map_free(writes);
+    isl_union_map_free(reads);
+    isl_union_map_free(writes);
     //isl_union_map_free(schedule);
     //isl_union_map_free(relation);
     isl_union_set_free(domain);
@@ -62,14 +62,49 @@ std::string ScopInfo::toString()
     s << "SCoPInfo: " << std::endl;
 
     s << "Domain: " << isl_union_set_to_str(domain) << std::endl;
-    // s << "Schedule: " << isl_union_map_to_str(schedule) << std::endl;
-    // s << "Relation: " << isl_union_map_to_str(relation) << std::endl;
-    // s << "Reads:  " << isl_union_map_to_str(reads) << std::endl;
-    // s << "Writes: " << isl_union_map_to_str(writes) << std::endl;
+    s << "Schedule: " << isl_union_map_to_str(schedule) << std::endl;
+    s << "Relation: " << isl_union_map_to_str(relation) << std::endl;
+    s << "Reads:  " << isl_union_map_to_str(reads) << std::endl;
+    s << "Writes: " << isl_union_map_to_str(writes) << std::endl;
     return s.str();
+}
+void computeFlow(
+        __isl_keep isl_union_map* r1, __isl_keep isl_union_map* r2,
+        __isl_keep isl_union_map* r3, __isl_keep isl_union_map* r4,
+        __isl_keep isl_union_map **computed_rel)
+{
+    isl_union_map_compute_flow(isl_union_map_copy(r1), isl_union_map_copy(r2),
+        isl_union_map_copy(r3), isl_union_map_copy(r4),
+        NULL, computed_rel, NULL, NULL);
+
 }
 
 void ScopInfo::computeRelationUnion() {
+    schedule = isl_schedule_get_map(pet->schedule);
+    reads = pet_scop_get_may_reads(pet);
+    writes = pet_scop_get_may_writes(pet);
+    isl_space *space = isl_set_get_space(pet->context);
+    isl_union_map *empty = isl_union_map_empty(space);
+
+    isl_union_map *dep_read_after_write;
+    isl_union_map *dep_write_after_read;
+    isl_union_map *dep_write_after_write;
+
+    computeFlow(reads, empty, writes, schedule, &dep_read_after_write);
+    computeFlow(writes, empty, reads, schedule, &dep_write_after_read);
+    computeFlow(writes, empty, writes, schedule, &dep_write_after_write);
+
+    dep_read_after_write = isl_union_map_coalesce(dep_read_after_write);
+    dep_write_after_read = isl_union_map_coalesce(dep_write_after_read);
+    dep_write_after_write = isl_union_map_coalesce(dep_write_after_write);
+    relation = dep_read_after_write;
+    //    relation = isl_union_map_union(relation, dep_write_after_read);
+    //   relation = isl_union_map_union(relation, dep_write_after_write);
+    relation = isl_union_map_coalesce(relation);
+    
+
+    isl_union_map_free(empty);
+    //relation = unwrap_range(relation);
 }
 
 void ScopInfo::normalize()
