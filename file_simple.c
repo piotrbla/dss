ca5cfaa8d702bf399fba2d5dab6018fa5e817692
Piotr B <piotrb@gmail.com>
2021-06-29 02:00:06 +0200

    file_simple example improved

--- file_simple.c
+++ file_simple.c
@@ -1,11 +1,10 @@
-void LMKernel6_01_Base(int loop, int n, int *input_w, int **input_b)
+void LMKernel6_01_Base(int loop, int n, int *w, int **a)
 {
-  int** a;// = get_vector_copy(input_w, n);
 #pragma scop
   
   int j, i;
     for (i = 1; i < n; i++) {
-      for (j = 0; j < i; j++) {
+      for (j = 1; j < n; j++) {
         a[i][j] = a[i-1][j]  + a[i][j-1];
       }
     }
