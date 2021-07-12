a6600eb3678b4afcf9be290b68969226df1a6ae4
Piotr B <piotrb@gmail.com>
2021-07-12 20:50:41 +0200

    Test fix - internal variable name

--- tests/file_dep_2.c
+++ tests/file_dep_2.c
@@ -7,7 +7,7 @@ void dep_2(int k1, int k2, int n)
     int i, j;
     for (i = max(2, k2); i < n; i++)
     {
-        for (j = max(2, k1); i < n; i++)
+        for (j = max(2, k1); j < n; j++)
         {
             a[i][j] = a[i][j - 1] + a[i][j - 2] + a[i][j - k1] + a[i - 2][j] + a[i - k2][j];
         }
