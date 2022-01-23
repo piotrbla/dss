e79b03fe623836d51ccee832df5391e52900c335
Piotr B <piotrb@gmail.com>
2022-01-23 17:33:13 +0100

    Modified tests

--- file_nus.c
+++ file_nus.c
@@ -6,8 +6,11 @@ void computeDYN1Imperfect(int** table, int n, int *seq) {
     for (int j = i + 1; j < n; j++) {
       for (int k = i; k < j; k++) {
         S[i][j] = max_score(S[i][k] + S[k+1][j], S[i][j]); // s1
+    	if (k==j-1)
+	{  
+      		S[i][j] = max_score(S[i][j], S[i+1][j-1] + sigma(i, j)); // s2
+	}
       }
-      S[i][j] = max_score(S[i][j], S[i+1][j-1] + sigma(i, j)); // s2
     }
   }
 #pragma endscop
